8b9c3de1c5eac1fe33177a43a6721ae01064ae17
Jinkal Parmar <73112186+j100k-parmar@users.noreply.github.com>
2021-02-23 00:02:45 +0530

    login4

---
+++ JavaScript/page3.js
@@ -0,0 +1,25 @@
+function demo() {
+    // document.querySelector("#login").style.backgroundColor = "gray";
+
+
+    document.querySelector("#signup").style.height = "560px";
+    document.querySelector("#signup").style.width = "400px";
+    document.querySelector("#register").style.display = "block";
+    document.querySelector("#signup").style.borderRadius = "0%";
+    document.querySelector("#signup").style.left = "0px";
+    document.querySelector("#signup").style.top = "-305px";
+
+
+}
+
+
+
+function btnclose() {
+    document.querySelector("#register").style.display = "none";
+    document.querySelector("#signup").style.height = "150px";
+    document.querySelector("#signup").style.width = "150px";
+    // document.querySelector("#signup").style.backgroundColor = "black";
+    document.querySelector("#signup").style.borderRadius = "50%";
+    document.querySelector("#signup").style.left = "320px";
+    document.querySelector("#signup").style.top = "-300px";
+}
